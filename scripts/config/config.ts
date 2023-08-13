5e2fbbb6d0f1db4b1222876de9bb4d111745f531
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-08-13 23:14:46 +0200

    feat: update config

--- scripts/config/config.ts
+++ scripts/config/config.ts
@@ -1,19 +1,19 @@
 export default {
-    factory: "0x6aBdDa34Fb225be4610a2d153845e09429523Cd2",
-    weth9: "0xD4949664cD82660AaE99bEdc034a0deA8A0bd517",
-    router: "0xFCd2Ce20ef8ed3D43Ab4f8C2dA13bbF1C6d9512F",
+    factory: "",
+    weth9: "",
+    router: "",
     mockUSDC: "",
     mockEVMOS: "",
     mockATOM: "",
     mockOSMOSIS: "",
-    multicall2: "0x2764b262E60D3Ac96999F7e73b0628B09CbB367E",
+    multicall2: "",
     LHS: "",
     RHS: "",
     testerAddress: "",
-    rewardToken: "0x3f75ceabCDfed1aCa03257Dc6Bdc0408E2b4b026",
+    rewardToken: "",
     secondaryRewardToken: "",
-    miniChef: "0x067eC87844fBD73eDa4a1059F30039584586e09d",
+    miniChef: "",
     complexRewarderTime: "",
-    xDiff: "0x75aeE82a16BD1fB98b11879af93AB7CE055f66Da",
-    diffusion: "0x3f75ceabCDfed1aCa03257Dc6Bdc0408E2b4b026"
+    xDiff: "",
+    diffusion: ""
 }
